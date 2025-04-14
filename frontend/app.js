d9f0fd297356a528307a0b02d0ffa743b7ed8236
kunalbankar18 <kunalbankar071@gmail.com>
2025-04-14 12:11:47 +0530

    aws ec2 deployment

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 
-const URL = 'http://backend:8000/api';
+const URL = 'http://13.53.98.220:8000/api';
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 app.get('/', async function (req, res) {
@@ -20,5 +20,5 @@ app.get('/', async function (req, res) {
 
 app.listen(3000, function () {
     console.log('App listening on port 3000!');
-    
+
 });
